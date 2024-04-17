bcf1d073382b0cf4d989406ec37f2b7d0bb94f8f
Alex Gavrusev <alex@gavrusev.dev>
2024-04-17 20:29:04 +0200

    fix: disable header and footer line length commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // do not error for long body and footer lines (like those seen in dependabot commits)
+    "body-max-line-length": [0],
+    "footer-max-line-length": [0],
+  },
+};
