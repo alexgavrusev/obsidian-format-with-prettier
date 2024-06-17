984912bd12db48a75c31a3743f18b883602190d3
Alex Gavrusev <alex@gavrusev.dev>
2024-06-17 20:32:24 +0200

    fix(release): resolve issue with missing compare url in release header

---
+++ .changelogrc.mjs
@@ -0,0 +1,17 @@
+import createPreset from "conventional-changelog-conventionalcommits";
+
+export default createPreset({
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "perf", section: "Performance Improvements" },
+    { type: "revert", section: "Reverts" },
+    { type: "docs", section: "Documentation" },
+    { type: "style", section: "Styles" },
+    { type: "chore", section: "Miscellaneous Chores" },
+    { type: "refactor", section: "Code Refactoring" },
+    { type: "test", section: "Tests" },
+    { type: "build", section: "Build System" },
+    { type: "ci", section: "Continuous Integration" },
+  ],
+});
